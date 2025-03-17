d28e75b53e33a88fbf777c960cc095f09f5ce82e
Pablo Darafeyeu <pauldarafeyeu@gmail.com>
2025-03-17 14:20:57 +0300

    create methods for working with the sheet have been created in the interface

--- src/main/java/org/example/my_list/my_array_list/list/MyList.java
+++ src/main/java/org/example/my_list/my_array_list/list/MyList.java
@@ -1,2 +1,50 @@
-package org.example.my_list.my_array_list.list;public interface MyList {
+package org.example.my_list.my_array_list.list;
+
+public interface MyList<T> {
+
+    /**
+     * Добавляет элемент в конец списка
+     *
+     * @param element элемент который вставим в лист
+     */
+    void add(T element);
+
+    /**
+     * Добавляет элемент по индексу
+     *
+     * @param index   - индекс а место которого будет вставлен элемент
+     * @param element - элемент который будет вставлен
+     */
+    void add(int index, T element);
+
+    /**
+     * Удаляет элемент по индексу после чего, сдвигает матрицу влево.
+     *
+     * @param index - индекс элемента который нужно удалить
+     */
+    void remove(int index);
+
+    /**
+     * Возвращает элемент Т по индексу
+     *
+     * @param index - индекс элемента, который нужно вернуть
+     * @return - возвращает элемент T по указанному индексу
+     */
+    T get(int index);
+
+    /**
+     * Заменяет элемент T по указанному индексу.
+     *
+     * @param index   - индекс элемента T, который нужно заменить
+     * @param element - элемент, который будет вставлен по указанному индексу
+     * @return - обновленный элемент T
+     */
+    T set(int index, T element);
+
+    /**
+     * Удаляет все элементы из данного списка.
+     */
+    void clear();
+
+
 }
