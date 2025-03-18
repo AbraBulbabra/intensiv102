b3d1f7311c065fb9615ae3c6b1af6b3cec034f28
Pablo Darafeyeu <pauldarafeyeu@gmail.com>
2025-03-18 17:45:45 +0300

    Исправил замечаниÐя

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,16 @@
 package org.example;
 
 import org.example.craft.exception.CraftException;
+import org.example.handling.HandlingException;
 
 public class Main {
     public static void main(String[] args) {
+        HandlingException handling = new HandlingException();
+        handling.errorHandling(false);
+        handling.errorHandling(true);
 
-        errorHendling(false);
-        errorHendling(true);
-
-        errorHendlingAndFinish(false);
-        errorHendlingAndFinish(true);
-        errorHendlingAndFinish(false);
-    }
-
-
-    private static void errorHendlingAndFinish(boolean b) {
-        try {
-            Work.checkLogic(b);
-            System.out.print("\nПрограмма продолжиться!");
-        } catch (CraftException e) {
-            System.out.printf(
-                    "\n Мы словили ошибку \n->%s\nпрограма закончена!", e.toString()
-            );
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private static void errorHendling(boolean b) {
-        try {
-            Work.checkLogic(b);
-            System.out.print("\nПрограмма продолжиться!");
-        } catch (CraftException e) {
-            System.out.printf(
-                    "\n------- Мы словили ошибку \n-> %s\n!!!но програму не остановили!", e.toString()
-            );
-        }
+        handling.errorHandlingAndFinish(false);
+        handling.errorHandlingAndFinish(true);
+        handling.errorHandlingAndFinish(false);
     }
 }
